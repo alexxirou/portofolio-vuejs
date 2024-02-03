0114a0b40283f23e093ec6b90e4635d632e604ca
alexxirou <alex.xirou@hotmail.com>
2024-02-03 22:06:59 +0200

    fixed image paths in ProjectsSection

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -36,6 +36,8 @@
  */
 import ProjectSlider from '../UI/ProjectSlider.vue'
 import ProjectSlide from './ProjectSlide.vue'
+import imageForkify from './../assets/images/forkify.png';
+import imageBattleship from './../assets/images/Battleship.png';
 import { ref, provide } from 'vue'
 
 const curSlide=ref<number>(0);
@@ -44,7 +46,7 @@ provide('curSlide', curSlide);
 const slides = [
   {
     title: 'Recipe Finder Web Application',
-    imageSrc: '/src/assets/images/forkify.png',
+    imageSrc: imageForkify,
     description:
       'The Recipe Finder Web Application is a fully functional and responsive web application that allows users to discover and explore a wide variety of recipes. It provides users with an intuitive and interactive interface to search for recipes, view detailed recipe information, adjust serving sizes, and bookmark their favorite recipes for quick access.',
     technologies: 'JavaScript (ES6+), Parcel Bundler, Forkify API',
@@ -54,7 +56,7 @@ const slides = [
   },
   {
     title: 'Battleship Game',
-    imageSrc: '/src/assets/images/Battleship.png',
+    imageSrc: imageBattleship,
     description:
       'A battleship game written in Ruby vs AI. It provides a GUI using tkinter for Ruby and Sinatra library to create API endpoints for a web UI.',
     technologies: 'Ruby, tk, sinstra, unicorn',
